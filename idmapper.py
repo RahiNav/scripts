b31a4cbc605bf6cdfc29340e917f86a1ea60e81e
Rahi Navelkar <43855181+Rahi13@users.noreply.github.com>
2021-05-28 12:38:30 -0400

    new files

---
+++ idmapper.py
@@ -0,0 +1,22 @@
+#The script uses panda to merge two file based on a anchor (common) column between the two file.
+# The merge type can be inner/left/right/outer. for secriptions please see https://www.geeksforgeeks.org/how-to-do-a-vlookup-in-python-using-pandas/
+
+import pandas as pd
+
+infile1='/Users/rsn13/Desktop/scripts/input/glycan_chebi.csv'
+infile2='/Users/rsn13/Desktop/scripts/input/glycan_xref_reactome.csv'
+outfile='/Users/rsn13/Desktop/scripts/output/mapped1.txt'
+anchor_column = 'glytoucan_ac'
+merge_type = 'inner'
+
+df1 = pd.read_csv(infile1)
+df2 = pd.read_csv(infile2)
+    
+inner_join = pd.merge(df1,df2,on=anchor_column,how=merge_type)
+
+sourceFile = open(outfile, 'a')
+print(inner_join, file=sourceFile)
+
+sourceFile.close()
+
+print('Done')
---
+++ idmapper_without_pandas.py
@@ -0,0 +1,43 @@
+# The script takes two csv files and maps the said column together based on the anhor(common)colum.
+# The script only workks when there is one-to-one mapping between the anchor column and said column in files
+#anchor_file1 and anchor_file2 needs to have the common column. same format. 
+
+import csv
+
+infile1='/Users/rsn13/Desktop/scripts/input/glycan_chebi.csv'
+infile2='/Users/rsn13/Desktop/scripts/input/glycan_xref_reactome.csv'
+outfile = '/Users/rsn13/Desktop/scripts/output/mapped.txt'
+
+dct1 ={}
+dct2 = {}
+
+anchor_file1 = 0
+anchor_file2 = 0 
+column_file1 = 1
+column_file2 = 1
+
+with open(infile1) as csv_file1:
+    csv_reader1 = csv.reader(csv_file1, delimiter=',')
+    
+    for line in csv_reader1:
+        dct1[line[anchor_file1]] = line[column_file1]
+    
+with open(infile2) as csv_file2:
+    csv_reader2 = csv.reader(csv_file2, delimiter=',')  
+    
+    for line in csv_reader2:
+        dct2[line[anchor_file2]] = line[column_file2]    
+    
+
+for anchor1,id1 in dct1.items():
+    sourceFile = open(outfile,'a')
+    for anchor2,id2 in dct2.items():
+        if anchor1 == anchor2:
+            print(anchor1,anchor2,id1,id2,file=sourceFile)
+
+sourceFile.close()            
+    
+csv_file1.close()
+csv_file2.close()
+
+print('Done')  
