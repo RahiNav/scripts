084ede97f044c63b4f1e1beb2f7cb9cae64481c9
Rahi Navelkar <43855181+Rahi13@users.noreply.github.com>
2021-06-07 11:11:50 -0400

    count frequency

---
+++ count_freqeuency.py
@@ -0,0 +1,36 @@
+# Takes a column from a file and calculates its frequency in the column. #prints the item and frequency in the output file. 
+# sorts the output file by highest frequency
+
+infile = '/Users/rsn13/Desktop/scripts/input/pubs.csv'
+outfile = '/Users/rsn13/Desktop/scripts/output/freq.txt'
+
+with open(infile) as file:
+    items=[]
+    frequency = {}
+    
+    for line in file:
+        line = line.strip()
+        items.append(line)
+                   
+    for item in items:
+        if item in frequency:
+            frequency[item] += 1
+        else:
+            frequency[item] = 1
+            
+    #print(frequency)
+    sort_orders = sorted(frequency.items(), key=lambda x: x[1], reverse=True)
+    
+    sourceFile = open(outfile,'w')
+    for i in sort_orders:
+        print(i[0], i[1], file=sourceFile)
+    
+    sourceFile.close()
+        
+print('Done')        
+
+
+            
+        
+        
+        
