f4e5feee86ca7d5cab4d45adea5deb5e30ecc806
Rahi Navelkar <89993916+RahiNav@users.noreply.github.com>
2022-06-04 14:17:44 -0400

    Update sort_dictionary.py

--- sort_dictionary.py
+++ sort_dictionary.py
@@ -1,3 +1,11 @@
+'''
+Challenge from Hacker Rank: https://www.hackerrank.com/challenges/most-commons/problem?isFullScreen=true
+Given a string , which is the company name in lowercase letters, your task is to find the top three most common characters in the string.
+Print the three most common characters along with their occurrence count.
+Sort in descending order of occurrence count.
+If the occurrence count is the same, sort the characters in alphabetical order.
+'''
+
 #!/bin/python3
 
 import math
